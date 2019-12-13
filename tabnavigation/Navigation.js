e8888ef5e199cd21bf68545d2cdbbbb66f00d767
dewyisle <dewyisle@gmail.com>
2019-12-14 08:59:01 +0900

    tabNavi design -ing

--- tabnavigation/Navigation.js
+++ tabnavigation/Navigation.js
@@ -1,25 +1,36 @@
 import React from "react";
+import { Text, View, StyleSheet, Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import SettingScreen from "../screens/SettingScreen";
+import { FontAwesome } from "@expo/vector-icons";
 
-const Tabnavigator = createBottomTabNavigator(
-  {
-    HomeScreen: {
-      screen: HomeScreen,
-      navigationOptions: { title: "Home" }
-    },
-    SettingScreen: {
-      screen: SettingScreen,
-      navigationOptions: { title: "Setting" }
-    }
+const Tabnavigator = createBottomTabNavigator({
+  HomeScreen: {
+    screen:HomeScreen,
+    navigationOptions: {title:"Calendar", tabBarIcon:<FontAwesome name={} size={}/>}
   },
-  {
-    tabBarOptions: {
-      style: {}
+  SettingScreen: {
+    screen:SettingScreen,
+    navigationOptions: {title:"List"}
     }
+},
+{
+  tabBarOptions: {
+    style:{
+        backgroundColor: "#c56cf0",
+        paddingBottom: 15
+    },
+    inactiveTintColor:"#f7f1e3",
+    activeTintColor : "#2c2c54",
+    labelStyle:{
+        fontSize:15
+    }
+    
+
   }
+}
 );
 
 export default createAppContainer(Tabnavigator);
