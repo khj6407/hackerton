719e885c49af1cb641c1cac46b9c737b4c30b9e1
dewyisle <dewyisle@gmail.com>
2019-12-14 00:15:28 +0900

    second calendar

--- components/Calendar/index.js
+++ components/Calendar/index.js
@@ -2,30 +2,29 @@ import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import * as Calendar from 'expo-calendar';
 
-class Calendar extends React.Component {
-    render(){
 
-        useEffect(() => {
-          (async () => {
-            const { status } = await Calendar.requestPermissionsAsync();
-            if (status === 'granted') {
-              const calendars = await Calendar.getCalendarsAsync();
-              console.log({ calendars });
-            }
-          })();
-        }, []);
-      
-        return (
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: '#fff',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text>Calendar Module Example</Text>
-          </View>
-        );
-    }
+const Calender = () => {
+    useEffect(() => {
+        (async () => {
+          const { status } = await Calendar.requestPermissionsAsync();
+          if (status === 'granted') {
+            const calendars = await Calendar.getCalendarsAsync();
+            console.log({ calendars });
+          }
+        })();
+      }, []);
+    
+      return (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text>Calendar Module Example</Text>
+        </View>
+      );
 }
-export default Calendar;
+
+export default Calender;
