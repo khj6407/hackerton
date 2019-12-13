f0c479df8e212f3fb2e64230509f7f472fe9185a
dewyisle <dewyisle@gmail.com>
2019-12-14 04:55:34 +0900

    tabnavigation

---
+++ tabnavigation/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text, View, StyleSheet, Platform } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import SettingScreen from "../screens/SettingScreen";
+
+const Tabnavigator = createBottomTabNavigator({
+  HomeScreen: {
+    screen:HomeScreen,
+    navigationOptions: {title:"Home"}
+  },
+  SettingScreen: {
+    screen:SettingScreen,
+    navigationOptions: {title:"Setting"}
+    }
+},
+{
+  tabBarOptions: {
+    style:{}
+  }
+}
+);
+
+export default createAppContainer(Tabnavigator);
