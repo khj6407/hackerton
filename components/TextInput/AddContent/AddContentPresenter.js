10f34e7f9181b128fa2adf5aa332eb9badc2b4d3
KimKyoungMin <gim181004@gmail.com>
2019-12-14 00:00:22 +0900

    input text

--- components/TextInput/AddContent/AddContentPresenter.js
+++ components/TextInput/AddContent/AddContentPresenter.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 class AddContent extends Component {
   render() {
     return (
-      <View>
-        <Text>wow</Text>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Contents.." />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  input: {
+    width: 300,
+    height: 300,
+    padding: 10,
+    marginTop: 20
+  }
+});
 
 export default AddContent;
