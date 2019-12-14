452af4eae0a491048a873519fac518b9f059b808
KimKyoungMin <gim181004@gmail.com>
2019-12-14 10:49:32 +0900

    last commit

--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -1,22 +1,60 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-class SettingScreen extends React.Component{
-  render(){
-    return(
-      <View style={styles.setscr}>
-        <Text>SettingScreen</Text>
+class SettingScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.conn}>
+          <FontAwesome name={"calendar-check-o"} size={43} />
+          <Text style={styles.text}>11월 30일</Text>
+        </View>
+        <View style={styles.con}>
+          <Text style={styles.con}>1️⃣ 해커톤.....</Text>
+          <Text style={styles.con}>2️⃣ 졸리다..</Text>
+          <Text style={styles.con}>3️⃣ 자도 자도 졸리다... </Text>
+        </View>
+        <View style={styles.conn}>
+          <FontAwesome name={"calendar-check-o"} size={43} />
+          <Text style={styles.text}>12월 1일</Text>
+        </View>
+        <View style={styles.con}>
+          <Text style={styles.con}>1️⃣ 집 가고 싶다... </Text>
+          <Text style={styles.con}>2️⃣ 300시간만 자고 싶다... </Text>
+          <Text style={styles.con}>3️⃣ 얼른 끝나라... </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  setscr:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center"
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    marginTop: 60,
+    marginLeft: 30
+  },
+  text: {
+    color: "#182C61",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginLeft: 20,
+    marginBottom: 20,
+    fontSize: 32
+  },
+  con: {
+    flexDirection: "column",
+    marginTop: 30,
+    marginBottom: 28,
+    fontSize: 25,
+    marginRight: 0
+  },
+  conn: {
+    flex: 1,
+    flexDirection: "row"
   }
-  });
+});
 
-  export default SettingScreen;
+export default SettingScreen;
